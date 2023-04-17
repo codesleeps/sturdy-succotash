96c0d78f3ba0837eda1dc90b17e0f95686b20781
Codesleep <125808990+codesleeps@users.noreply.github.com>
2023-04-17 23:27:18 +0100

    completed project for marking will add clock later

--- assets/js/app.js
+++ assets/js/app.js
@@ -76,57 +76,130 @@ $(document).ready(function () {
 		$.ajax({
 			url: queryURL,
 			method: 'GET',
-		}).then(function (response) {
-			coords.push(response.coord.lat);
-			coords.push(response.coord.lon);
-			let cityName = response.name;
-			let cityCond = response.weather[0].description.toUpperCase();
-			let cityTemp = response.main.temp;
-			let cityHum = response.main.humidity;
-			let cityWind = response.wind.speed;
-			let icon = response.weather[0].icon;
-			$('#icon').html(
-				`<img src="http://openweathermap.org/img/wn/${icon}@2x.png">`
-			);
-			$('#city-name').html(cityName + ' ' + '(' + NowMoment + ')');
-			$('#city-cond').text('Current Conditions: ' + cityCond);
-			$('#temp').text('Current Temp (C): ' + cityTemp.toFixed(1));
-			$('#humidity').text('Humidity: ' + cityHum + '%');
-			$('#wind-speed').text('Wind Speed: ' + cityWind + 'mph');
-			$('#dateOne').text(dayOne);
-			$('#dateTwo').text(dayTwo);
-			$('#dateThree').text(dayThree);
-			$('#dateFour').text(dayFour);
-			$('#dateFive').text(dayFive);
+		})
+			.then(function (response) {
+				coords.push(response.coord.lat);
+				coords.push(response.coord.lon);
+				let cityName = response.name;
+				let cityCond = response.weather[0].description.toUpperCase();
+				let cityTemp = response.main.temp;
+				let cityHum = response.main.humidity;
+				let cityWind = response.wind.speed;
+				let icon = response.weather[0].icon;
+				$('#icon').html(
+					`<img src="http://openweathermap.org/img/wn/${icon}@2x.png">`
+				);
+				$('#city-name').html(cityName + ' ' + '(' + NowMoment + ')');
+				$('#city-cond').text('Current Conditions: ' + cityCond);
+				$('#temp').text('Current Temp (C): ' + cityTemp.toFixed(1));
+				$('#humidity').text('Humidity: ' + cityHum + '%');
+				$('#wind-speed').text('Wind Speed: ' + cityWind + 'mph');
+				$('#dateOne').text(dayOne);
+				$('#dateTwo').text(dayTwo);
+				$('#dateThree').text(dayThree);
+				$('#dateFour').text(dayFour);
+				$('#dateFive').text(dayFive);
 
+				getUV(response.coord.lat, response.coord.lon);
+			})
+			.fail(function () {
+				alert('Could not get data');
+			});
 
-      getUV(response.coord.lat, response.coord.lon);
-    }).fail(function (){
-      alert("Could not get data")
-    });
+		function getUV(lat, lon) {
+			$.ajax({
+				url:
+					'https://api.openweathermap.org/data/2.5/onecall?lat=' +
+					lat +
+					'&lon=' +
+					lon +
+					'&exclude=minutely,hourly' +
+					'&units=metric&appid=42aab93f919a0108de29c04395767465',
+				method: 'GET',
+			}).then(function (response) {
+				//code to determine UV index 10.07 🙄
+				let uvIndex = response.current.uvi;
+				$('#uv-index').text('UV Index:' + ' ' + uvIndex);
+				if (uvIndex >= 8) {
+					$('#uv-index').css('color', 'red');
+				} else if (uvIndex > 4 && uvIndex < 8) {
+					$('#uv-index').css('color', 'yellow');
+				} else {
+					$('#uv-index').css('color', 'lime');
+				}
+				let cityHigh = response.daily[0].temp.max;
+				$('#high').text('Expected high (C): ' + ' ' + cityHigh);
 
+				//forecast temp variables
+				let dayOnetemp = response.daily[1].temp.max;
+				let dayTwotemp = response.daily[2].temp.max;
+				let dayThreetemp = response.daily[3].temp.max;
+				let dayFourtemp = response.daily[4].temp.max;
+				let dayFivetemp = response.daily[5].temp.max;
+				//forecast humidity variables
+				let dayOnehum = response.daily[1].humidity;
+				let dayTwohum = response.daily[2].humidity;
+				let dayThreehum = response.daily[3].humidity;
+				let dayFourhum = response.daily[4].humidity;
+				let dayFivehum = response.daily[5].humidity;
+				//forecast weather icon variables
+				let iconOne = response.daily[1].weather[0].icon;
+				let iconTwo = response.daily[2].weather[0].icon;
+				let iconThree = response.daily[3].weather[0].icon;
+				let iconFour = response.daily[4].weather[0].icon;
+				let iconFive = response.daily[5].weather[0].icon;
 
-    function getUV(lat, lon) {
-      $.ajax({
-         url: "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly" + "&units=metric&appid=42aab93f919a0108de29c04395767465",
-         method: "GET",
-      }).then(function(response){
-        //code to determine UV index 10.07 🙄
-        let uvIndex = response.current.uvi;
-        $("#uv-index").text("UV Index:" + " " + uvIndex);
-        if (uvIndex >= 8) {
-          $("#uv-index").css("color", "red");
-        } else if (uvIndex > 4 && uvIndex < 8) {
-          $("#uv-index").css("color", "yellow");
-        } else {
-          $("#uv-index").css("color", "lime");
-        }
-        let cityHigh = response.daily[0].temp.max;
-        $("#high").text("Expected high (C): " + " " + cityHigh);
+				// forecast temperature
+				$('#tempOne').text('Temp(C):' + ' ' + dayOnetemp.toFixed(1));
+				$('#tempTwo').text('Temp(C):' + ' ' + dayTwotemp.toFixed(1));
+				$('#tempThree').text('Temp(C):' + ' ' + dayThreetemp.toFixed(1));
+				$('#tempFour').text('Temp(C):' + ' ' + dayFourtemp.toFixed(1));
+				$('#tempFive').text('Temp(C):' + ' ' + dayFivetemp.toFixed(1));
+				// forecast humidity
+				$('#humOne').text('Hum:' + ' ' + dayOnehum + '%');
+				$('#humTwo').text('Hum:' + ' ' + dayTwohum + '%');
+				$('#humThree').text('Hum:' + ' ' + dayThreehum + '%');
+				$('#humFour').text('Hum:' + ' ' + dayFourhum + '%');
+				$('#humFive').text('Hum:' + ' ' + dayFivehum + '%');
 
-      })
-    }
+				$('#iconOne').html(
+					`<img src="http://openweathermap.org/img/wn/${iconOne}@2x.png">`
+				);
+				$('#iconTwo').html(
+					`<img src="http://openweathermap.org/img/wn/${iconTwo}@2x.png">`
+				);
+				$('#iconThree').html(
+					`<img src="http://openweathermap.org/img/wn/${iconThree}@2x.png">`
+				);
+				$('#iconFour').html(
+					`<img src="http://openweathermap.org/img/wn/${iconFour}@2x.png">`
+				);
+				$('#iconFive').html(
+					`<img src="http://openweathermap.org/img/wn/${iconFive}@2x.png">`
+				);
+			});
+		}
+	}
+	//function to render recently searched cities to page
+	function listCities() {
+		$('#cityList').text('');
+		cities.forEach((city) => {
+			$('#cityList').prepend('<tr><td>' + city + '</td></tr>');
+		});
+	}
 
-
-
-}});
+	listCities();
+	//event handler for recently searched cities in table
+	$(document).on('click', 'td', (e) => {
+		e.preventDefault();
+		let listedCity = $(e.target).text();
+		city = listedCity;
+		search();
+	});
+	//event handler for clear button
+	$('#clr-btn').click(() => {
+		localStorage.removeItem('cities');
+		loadRecentCities();
+		listCities();
+	});
+});
